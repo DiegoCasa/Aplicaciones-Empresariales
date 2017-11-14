4f1f62032fd4077eb9a85d6a6898a0153fa40ca7
Diego <diego.casa@tecsup.edu.pe>
2017-11-14 10:55:49 -0500

    Agregamos Buscar

--- Lab13/Lab13/Controllers/PersonaController.cs
+++ Lab13/Lab13/Controllers/PersonaController.cs
@@ -16,39 +16,7 @@ namespace Lab13.Controllers
             return View();
         }
 
-        public ActionResult Listar()
-        {
-            List<Persona> personas = new List<Persona>();
-            personas.Add(new Persona
-            {
-                PersonaID = 1,
-                Nombre = "Juan",
-                Apellido = "Perez",
-                Direccion = "Av. Evergreen 123",
-                FechaNac = Convert.ToDateTime("1997-11-07"),
-                Email = "juan@mail.com"
-            });
-            personas.Add(new Persona
-            {
-                PersonaID = 2,
-                Nombre = "Maria",
-                Apellido = "Salas",
-                Direccion = "Av. Progreso 325",
-                FechaNac = Convert.ToDateTime("1995-10-28"),
-                Email = "maria@mail.com"
-            });
-            personas.Add(new Persona
-            {
-                PersonaID = 3,
-                Nombre = "Carlos",
-                Apellido = "Martinez",
-                Direccion = "Av. Evemazanos 101",
-                FechaNac = Convert.ToDateTime("1982-02-14"),
-                Email = "carlos@mail.com"
-            });
-            return View(personas);
-        }
-        public ActionResult Mostrar(int id)
+        public ActionResult Listar(string Buscar)
         {
             List<Persona> personas = new List<Persona>();
             personas.Add(new Persona
@@ -79,10 +47,16 @@ namespace Lab13.Controllers
                 Email = "carlos@mail.com"
             });
 
-            Persona persona = (from p in personas
-                               where p.PersonaID == id
-                               select p).FirstOrDefault();
-            return View(persona);
+            var persona = from p in personas select p;
+
+            if (!String.IsNullOrEmpty(Buscar))
+            {
+                persona = personas.Where(p => p.Apellido.Contains(Buscar) || p.Nombre.Contains(Buscar));
+            }
+
+            return View(persona.ToList());
         }
+
+        
     }
     }
